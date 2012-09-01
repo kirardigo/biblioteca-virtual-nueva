593eecd3d51d37c6bcd26610677b44ea2317e877
Gabriel <gabrielfigueroa06@gmail.com>
2012-09-01 20:50:15 -0300

    dsadas

--- apps/frontend/modules/contenido/templates/indexSuccess.php
+++ apps/frontend/modules/contenido/templates/indexSuccess.php
@@ -1,11 +1,12 @@
 <h1>Contenidos List</h1>
 
-<table>
-  <thead>
+<table class="table table-bordered">
+  <thead style="background: #7FDDCA">
     <tr>
       <th>Id contenido</th>
       <th>Numero contenido</th>
       <th>Nombre</th>
+      <th>Acciones</th>
     </tr>
   </thead>
   <tbody>
@@ -14,9 +15,12 @@
       <td><a href="<?php echo url_for('contenido/edit?id_contenido='.$Contenido->getIdContenido()) ?>"><?php echo $Contenido->getIdContenido() ?></a></td>
       <td><?php echo $Contenido->getNumeroContenido() ?></td>
       <td><?php echo $Contenido->getNombre() ?></td>
+            <td>          
+          <a class="btn btn-warning btn-mini" href="<?php echo url_for('contenido/edit?id_contenido='.$Contenido->getIdContenido()) ?>"><i class="icon-pencil icon-white"></i>Modificar</a>
+          <?php echo link_to('<i class="icon-trash icon-white"></i>Eliminar', 'contenido/delete?id_material='.$Contenido->getIdContenido(), array('method' => 'delete', 'confirm' => 'Esta seguro de eliminar el contenido?', 'class'=>"btn btn-danger btn-mini")) ?>
+      </td>
     </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
-
-  <a href="<?php echo url_for('contenido/new') ?>">New</a>
+<a class="btn btn-success" href="<?php echo url_for('contenido/new') ?>"><i class="icon-fire icon-white"></i>Agregar</a>
--- apps/frontend/modules/subcontenido/templates/indexSuccess.php
+++ apps/frontend/modules/subcontenido/templates/indexSuccess.php
@@ -1,12 +1,13 @@
 <h1>Subcontenidos List</h1>
 
-<table>
-  <thead>
+<table class="table table-bordered">
+  <thead style="background: #7FDDCA">
     <tr>
       <th>Id subcontenido</th>
       <th>Nombre</th>
       <th>Numero subcontenido</th>
       <th>Contenido id contenido</th>
+      <th>Acciones</th>
     </tr>
   </thead>
   <tbody>
@@ -16,9 +17,13 @@
       <td><?php echo $Subcontenido->getNombre() ?></td>
       <td><?php echo $Subcontenido->getNumeroSubcontenido() ?></td>
       <td><?php echo $Subcontenido->getContenidoIdContenido() ?></td>
+      <td>          
+          <a class="btn btn-warning btn-mini" href="<?php echo url_for('subcontenido/edit?id_subcontenido='.$Subcontenido->getIdSubcontenido()) ?>"><i class="icon-pencil icon-white"></i>Modificar</a>
+          <?php echo link_to('<i class="icon-trash icon-white"></i>Eliminar', 'subcontenido/delete?id_subcontenido='.$Subcontenido->getIdSubcontenido(), array('method' => 'delete', 'confirm' => 'Esta seguro de eliminar el subcontenido?', 'class'=>"btn btn-danger btn-mini")) ?>
+      </td>
     </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
 
-  <a href="<?php echo url_for('subcontenido/new') ?>">New</a>
+<a class="btn btn-success" href="<?php echo url_for('subcontenido/new') ?>"><i class="icon-fire icon-white"></i>Agregar</a>
